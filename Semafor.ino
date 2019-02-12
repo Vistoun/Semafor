5025546760fef21defdbd5f6464a2236b2952469
Vistoun <lachta.vasek@gmail.com>
2019-02-12 17:22:00 +0100

    Semafor

---
+++ Semafor.ino
@@ -0,0 +1,35 @@
+
+int cervena= 7;
+int zluta= 6;
+int zelena= 5;
+int btn= 4;
+
+void setup() {
+pinMode(cervena, OUTPUT);
+pinMode(zluta, OUTPUT);
+pinMode(zelena, OUTPUT);
+pinMode(btn, INPUT);
+  
+}
+
+void loop() {
+if(digitalRead(btn) == HIGH){
+digitalWrite(zelena, LOW);
+digitalWrite(zluta, LOW);
+digitalWrite(cervena, HIGH);
+delay(10000);
+digitalWrite(cervena, HIGH);
+digitalWrite(zluta, HIGH);
+digitalWrite(zelena, LOW);
+delay(1500);
+digitalWrite(zelena, HIGH);
+digitalWrite(cervena, LOW);
+digitalWrite(zluta, LOW);
+delay(5000);
+  }
+   else{
+    digitalWrite(cervena, LOW);
+    digitalWrite(zluta, LOW);
+    digitalWrite(zelena, LOW);
+   }
+}
