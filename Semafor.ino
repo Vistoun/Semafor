f19c7120c6f53aa55d7e9df7a84872fe5df276ad
Vistoun <47534064+Vistoun@users.noreply.github.com>
2019-02-14 10:48:11 +0100

    Update Semafor.ino

--- Semafor.ino
+++ Semafor.ino
@@ -13,23 +13,30 @@ pinMode(btn, INPUT);
 }
 
 void loop() {
-if(digitalRead(btn) == HIGH){
- digitalWrite(zelena, LOW);
- digitalWrite(zluta, LOW);
- digitalWrite(cervena, HIGH);
- delay(10000);
- digitalWrite(cervena, HIGH);
- digitalWrite(zluta, HIGH);
- digitalWrite(zelena, LOW);
- delay(1500);
- digitalWrite(zelena, HIGH);
- digitalWrite(cervena, LOW);
- digitalWrite(zluta, LOW);
- delay(5000);
-  }
+  if(digitalRead(btn) == HIGH){
+     digitalWrite(zelena, LOW);
+     digitalWrite(zluta, LOW);
+     digitalWrite(cervena, HIGH);
+     delay(10000);
+     digitalWrite(cervena, HIGH);
+     digitalWrite(zluta, HIGH);
+     digitalWrite(zelena, LOW);
+     delay(1500);
+     digitalWrite(zelena, HIGH);
+     digitalWrite(cervena, LOW);
+     digitalWrite(zluta, LOW);
+     delay(5000);
+     digitalWrite(cervena, LOW);
+     digitalWrite(zelena, HIGH);
+     digitalWrite(zluta, HIGH);
+     delay(1500);
+     digitalWrite(cervena, HIGH);
+     digitalWrite(zluta, LOW);
+     digitalWrite(zelena, LOW);
+    }
    else{
-    digitalWrite(cervena, LOW);
-    digitalWrite(zluta, LOW);
-    digitalWrite(zelena, LOW);
-   }
+      digitalWrite(cervena, LOW);
+      digitalWrite(zluta, LOW);
+      digitalWrite(zelena, LOW);
+     }
 }
